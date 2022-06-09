eb97ba306576fb56582fecf7cd75820c08804c36
SteveDutch <steholl@posteo.org>
2022-06-09 10:08:47 +0200

    make threads run

--- src/com/coderscampus/stefanassignment8/StartApp.java
+++ src/com/coderscampus/stefanassignment8/StartApp.java
@@ -23,79 +23,24 @@ public class StartApp {
 		Executor executor = Executors.newSingleThreadScheduledExecutor();
 		//CompletableFuture.runAsync(()-> assignment.getNumbers(), executor);
 		 			
-		// from week 12-09r 
-//		for (int j=0; j<1000; j++) {
-//			tasks = ((CompletableFuture) CompletableFuture.supplyAsync(() ->new Assignment8(), ioBoundTask)
-//					.thenApplyAsync(assignment -> assignment.getNumbers(), cpuBoundTask).get())
-//					.exceptionally( e -> "Pirat Guybrush hat den Todesfluch '"    );
-			
-			
 		// new try
 			for (int i = 0; i < 1000; i++) {
-				CompletableFuture<Void> task = CompletableFuture.supplyAsync(() -> assignment.getNumbers(), cpuBoundTask)
-																.thenAccept(number -> numbers.addAll(number));
-				tasks.add(task);
-			
-
-//		CompletableFuture.supplyAsync(() ->new Assignment8(), ioBoundTask)
-//		 .thenApplyAsync(assignment -> assignment.getNumbers(), cpuBoundTask)
-//		
-//		 .thenAcceptAsync(number -> numbers.addAll(numbers), cpuBoundTask);
-//		
+				CompletableFuture<Void> task = CompletableFuture
+												.supplyAsync(() -> assignment.getNumbers(), ioBoundTask)
+												.thenAccept(number -> numbers.addAll(number));
 		
 		System.out.println("Thead-"+ Thread.currentThread().getName()); 
 
-		
 		taskId++;
 		System.out.println("Hey look at me, I'm task-id: " + taskId + "! "
-				+ "I'm running on thread: " + Thread.currentThread().getName());
+				+ "I'm running on thread: " + Thread.currentThread().getName() + "\n DONE");
 		
-		String message = "Done";
-		System.out.println(message);
+
 		System.out.println("Länge von numbers = " + numbers.toArray().length);
 		
 		}
-//		CompletableFuture.runAsync(()-> assignment.getNumbers(), ioBoundTask);
-		
-//		for (int i=0; i<1000; i++) {
-//			
-//			int taskId = 0;
-//
-//    		String message = "Starting";
-//    		
-//    		CompletableFuture.runAsync(()-> assignment.getNumbers(), executor);
-//
-//    		System.out.println("Thead-"+ Thread.currentThread().getName()); 
-//			
-//    		
-//    	 	List<Integer> numbersList = assignment.getNumbers();
-//    	 	
-//    		System.out.println("Thead-"+ Thread.currentThread().getName()); 
-//    		System.out.println("round : " + i+1);
-//    		
-//    		taskId++;
-//    		System.out.println("Hey look at me, I'm task-id: " + taskId + "! "
-//    				+ "I'm running on thread: " + Thread.currentThread().getName());
-//    		
-//    		message = "Done";
-//    		System.out.println(message);
-//		}
-//		
 
-
-//	       
-//        for (int k=0; k<1000; k++) {
-//    		String message = "Starting";
-//    		System.out.println(message);
-//    		System.out.println("Thead-"+ Thread.currentThread().getName()); 
-//    		
-//        	List<Integer> numbersList = assignment.getNumbers();
-//        	
-//    		message = "Done";
-//    		System.out.println(message);
-//        	
-//        }
 	}
-	}
+}
 
 
