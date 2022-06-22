b472fcba7cb297c4c7da3328cbf5d9e5e1604b6b
SteveDutch <steholl@posteo.org>
2022-06-22 16:04:30 +0200

    use new Class ThreadRunner

--- src/com/coderscampus/stefanassignment8/StartApp.java
+++ src/com/coderscampus/stefanassignment8/StartApp.java
@@ -13,35 +13,9 @@ import java.util.stream.Collectors;
 public class StartApp {
 
 	public static void main(String[] args) throws InterruptedException {
+		ThreadRunner start = new ThreadRunner();
+		System.out.println(start.run() + " ;)");
 
-		Assignment8 assignment = new Assignment8();
-		List<Integer> numbers = Collections.synchronizedList(new ArrayList<>());
-
-		List<CompletableFuture<Void>> tasks = new ArrayList<>();
-
-		ExecutorService cpuBoundTask = Executors.newFixedThreadPool(1000);
-//		ExecutorService ioBoundTask = Executors.newCachedThreadPool();
-//		Executor executor = Executors.newSingleThreadScheduledExecutor();
-
-		// do like Trevor did
-		for (int i = 0; i < 1000; i++) {
-			CompletableFuture<Void> task = CompletableFuture.supplyAsync(() -> assignment.getNumbers(), cpuBoundTask)
-					.thenAccept(number -> numbers.addAll(number));
-			tasks.add(task);
-
-//		System.out.println("Thead-"+ Thread.currentThread().getName()); 
-
-		}
-
-		while (tasks.stream().filter(CompletableFuture::isDone).count() < 1000) {
-			// this just loops and keeps the main thread alive
-			// until all threads are done working. (Trevor is saying)
-		}
-
-		System.out.println("Länge von numbers = " + numbers.toArray().length);
-
-		Map<Integer, Long> numbersFrequency = numbers.stream()
-				.collect(Collectors.groupingBy(Function.identity(), Collectors.counting()));
-		System.out.println(numbersFrequency);
+		
 	}
 }
