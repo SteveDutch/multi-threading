c044add9c4e2770f31840f0df4925ca6d825cf1c
SteveDutch <steholl@posteo.org>
2022-06-22 15:39:36 +0200

    finally running

--- src/com/coderscampus/stefanassignment8/StartApp.java
+++ src/com/coderscampus/stefanassignment8/StartApp.java
@@ -3,44 +3,45 @@ package com.coderscampus.stefanassignment8;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.Map;
 import java.util.concurrent.CompletableFuture;
-import java.util.concurrent.Executor;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 
 public class StartApp {
 
-	public static void main(String[] args) {
-		
+	public static void main(String[] args) throws InterruptedException {
+
 		Assignment8 assignment = new Assignment8();
 		List<Integer> numbers = Collections.synchronizedList(new ArrayList<>());
-		int taskId = 0;
+
 		List<CompletableFuture<Void>> tasks = new ArrayList<>();
-		
-		
-		ExecutorService cpuBoundTask = Executors.newFixedThreadPool(300);
-		ExecutorService ioBoundTask = Executors.newCachedThreadPool();
-		Executor executor = Executors.newSingleThreadScheduledExecutor();
-		//CompletableFuture.runAsync(()-> assignment.getNumbers(), executor);
-		 			
-		// new try
-			for (int i = 0; i < 1000; i++) {
-				CompletableFuture<Void> task = CompletableFuture
-												.supplyAsync(() -> assignment.getNumbers(), ioBoundTask)
-												.thenAccept(number -> numbers.addAll(number));
-		
-		System.out.println("Thead-"+ Thread.currentThread().getName()); 
 
-		taskId++;
-		System.out.println("Hey look at me, I'm task-id: " + taskId + "! "
-				+ "I'm running on thread: " + Thread.currentThread().getName() + "\n DONE");
-		
+		ExecutorService cpuBoundTask = Executors.newFixedThreadPool(1000);
+//		ExecutorService ioBoundTask = Executors.newCachedThreadPool();
+//		Executor executor = Executors.newSingleThreadScheduledExecutor();
 
-		System.out.println("Länge von numbers = " + numbers.toArray().length);
-		
+		// do like Trevor did
+		for (int i = 0; i < 1000; i++) {
+			CompletableFuture<Void> task = CompletableFuture.supplyAsync(() -> assignment.getNumbers(), cpuBoundTask)
+					.thenAccept(number -> numbers.addAll(number));
+			tasks.add(task);
+
+//		System.out.println("Thead-"+ Thread.currentThread().getName()); 
+
 		}
 
+		while (tasks.stream().filter(CompletableFuture::isDone).count() < 1000) {
+			// this just loops and keeps the main thread alive
+			// until all threads are done working. (Trevor is saying)
+		}
+
+		System.out.println("Länge von numbers = " + numbers.toArray().length);
+
+		Map<Integer, Long> numbersFrequency = numbers.stream()
+				.collect(Collectors.groupingBy(Function.identity(), Collectors.counting()));
+		System.out.println(numbersFrequency);
 	}
 }
-
-
